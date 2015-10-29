e08c97444f209f0e95c6bed6e5887bd1268379c3
erzya <erzyasd@gmail.com>
2015-10-29 10:44:05 +0200

    temp working backend controller

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -3,10 +3,15 @@ package com.controller;
 
 
 import com.model.Item;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by erzyasd on 22.10.15.
@@ -16,12 +21,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 
     @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String listAll(Model model){
-        Item item = new Item();
-        item.setName("BRRRRRRRRRRR");
-
-        model.addAttribute("itemn", item);
+    public String begin(Model model){
         return "index";
+    }
+
+    @RequestMapping(value = "/item",method = RequestMethod.GET)
+    public @ResponseBody
+    List<Item> listAll(Model model){
+        List<Item> list = new ArrayList<Item>();
+
+        Item item1 = new Item();
+        item1.setName("BRRRRRRRRRRR");
+        Item item2 = new Item();
+        item2.setName("2BRRRR");
+        Item item3 = new Item();
+        item3.setName("3BR");
+
+        list.add(item1);
+        list.add(item2);
+        list.add(item3);
+
+        return list;
     }
 
 }
